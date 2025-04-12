eaf8cfa2db7761696787635151b8f84a3fa44072
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-04-12 15:25:38 +0000

    dosa bena na hai - bye

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+app.use(express.static(path.join(__dirname,'public')));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname,'/home.html'));
 })
-
+app.get('/signup',(req,res)=>{
+  res.send('LOet do it');
+})
+app.get('/signin',(req,res)=>{
+  res.send('LOet do it');
+})
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
