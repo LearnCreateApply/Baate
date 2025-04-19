44e397d75d204437ef6198c72ff90c6bd78bb682
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-04-19 04:37:30 +0000

    10:08 AM 19th April 2025

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const ejs = require('ejs')
 const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
 const e = require('express');
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/', (req, res) => {
@@ -15,21 +17,21 @@ app.get('/signup',(req,res)=>{
 app.get('/signin',(req,res)=>{
   res.sendFile(path.join(__dirname,'/templates/signin.html'));
 })
-app.post('/submit-signin',(req,res)=>{
+app.post('/signin',(req,res)=>{
   const {login,password } = req.body;
   fs.readFile('login.txt',(e,data) =>{
     if (data.includes(login) && data.includes(password)) {
-      res.send('Success')
+      ejs.render('userprofile',{username : `${login}`})
     }
     else{
-      res.send('Invalid')
+      res.send('Wrong email or password')
     }
   })
 })
-app.post('/submit-signup',(req,res)=>{
+app.post('/signup',(req,res)=>{
   const {email ,username,password } = req.body;
   fs.appendFile('login.txt',`email ${email} username ${username} password ${password}`,()=>{})
-  res.send("User Added")
+  res.redirect('/signin')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
