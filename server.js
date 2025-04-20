9b640168b09942a7cdfaceea8b2fae120186685e
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-04-20 14:53:54 +0000

    Final deploy

--- server.js
+++ server.js
@@ -1,38 +1,46 @@
 const express = require('express');
-const ejs = require('ejs')
-const app = express();
-const port = 3000;
+const http = require('http');
+const { Server } = require('socket.io');
 const path = require('path');
-const fs = require('fs');
-const e = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 3000;
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Socket.IO setup
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data); // Broadcast the message with username
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'/home.html'));
-})
-app.get('/signup',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/templates/signup.html'));
-})
-app.get('/signin',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/templates/signin.html'));
-})
-app.post('/signin',(req,res)=>{
-  const {login,password } = req.body;
-  fs.readFile('login.txt',(e,data) =>{
-    if (data.includes(login) && data.includes(password)) {
-      ejs.render('userprofile',{username : `${login}`})
-    }
-    else{
-      res.send('Wrong email or password')
-    }
-  })
-})
-app.post('/signup',(req,res)=>{
-  const {email ,username,password } = req.body;
-  fs.appendFile('login.txt',`email ${email} username ${username} password ${password}`,()=>{})
-  res.redirect('/signin')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+  res.sendFile(path.join(__dirname, '/home.html'));
+});
+
+app.get('/username', (req, res) => {
+  res.sendFile(path.join(__dirname, '/templates/username.html'));
+});
+
+app.post('/baate', (req, res) => {
+  const { login } = req.body;
+  res.render('userprofile', { username: login });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
